885424f946f7b74440092428ae6d4477caf29425
sjagadee <sjagadee@hawk.iit.edu>
2018-01-10 10:55:41 -0600

    Adding form

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// connection to db through mongoose
+mongoose.connect('mongodb://localhost/nodekb', { useMongoClient: true });
+let db = mongoose.connection;
+
+// check connection
+db.once('open', function(){
+  console.log('connected to mongodb');
+});
+
+//check for db error
+db.on('error', function(err){
+  console.log(err);
+});
 
 // Init app
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // Home route
 app.get('/', function(req, res) {
-
-  let articles = [
-    {
-      id:1,
-      title:'article one',
-      author:'Srini',
-      body:'this is article one'
-    },
-    {
-      id:2,
-      title:'article two',
-      author:'Maru',
-      body:'this is article two'
-    },
-    {
-      id:3,
-      title:'article three',
-      author:'Srini',
-      body:'this is article three'
-    },
-  ]
-
-  res.render('index', {
-    title:'Article',
-    articles: articles
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log();
+    } else {
+      res.render('index', {
+        title:'Article',
+        articles: articles
+      });
+    }
   });
 });
 
@@ -43,6 +52,23 @@ app.get('/articles/add', function(req, res){
   res.render('add_article', {
     title:'Add Article'
   });
+});
+
+// Add Submit POST route (submit button)
+app.post('/articles/add', function(req, res){
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  })
 });
 
 // Start server
