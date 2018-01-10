45849869586de777b4e776b9d73a9cc0dfe830fb
sjagadee <sjagadee@hawk.iit.edu>
2018-01-09 22:14:03 -0600

    Adding pug and commenting and many more

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+const express = require('express');
+const path = require('path');
+
+// Init app
+const app = express();
+
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home route
+app.get('/', function(req, res) {
+  res.render('index', {
+    title:'Article'
+  });
+});
+
+// Add route
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title:'Add Article'
+  });
+});
+
+// Start server
+app.listen(3003, function(){
+  console.log('Server Started on port 3003...');
+});
