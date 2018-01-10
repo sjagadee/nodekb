fa3838ef8bb92eb5605391b8b40a53db50a5c366
sjagadee <sjagadee@hawk.iit.edu>
2018-01-10 07:58:49 -0600

    print on screen

--- app.js
+++ app.js
@@ -10,8 +10,31 @@ app.set('view engine', 'pug');
 
 // Home route
 app.get('/', function(req, res) {
+
+  let articles = [
+    {
+      id:1,
+      title:'article one',
+      author:'Srini',
+      body:'this is article one'
+    },
+    {
+      id:2,
+      title:'article two',
+      author:'Maru',
+      body:'this is article two'
+    },
+    {
+      id:3,
+      title:'article three',
+      author:'Srini',
+      body:'this is article three'
+    },
+  ]
+
   res.render('index', {
-    title:'Article'
+    title:'Article',
+    articles: articles
   });
 });
 
